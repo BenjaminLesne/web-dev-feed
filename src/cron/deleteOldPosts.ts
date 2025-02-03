412694fa80eb4628b9f5cffd69ef1cb447b5da85
Benjamin Lesne <benjamin.lesne@outlook.fr>
2025-02-03 20:27:42 +0100

    fix: cron job error

--- src/cron/deleteOldPosts.ts
+++ src/cron/deleteOldPosts.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { deleteOldPosts } from '../database/queries.js';
 
-export const deleteOldPostsCronJob = cron.schedule('0 0 1/2 * *', () =>  {
+export const deleteOldPostsCronJob = cron.schedule('0 0 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 * *', () =>  {
     deleteOldPosts()
 }, {
   scheduled: false
